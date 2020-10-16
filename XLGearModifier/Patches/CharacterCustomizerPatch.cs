e9825ff725e028f48cdc6dbcf09a6515e3ad0078
Bryan Taylor <mcbtay@gmail.com>
2020-10-15 20:58:38 -0400

    initial commit.  a quick and dirty mod that will allow you to apply multiple gear items in a single slot, i.e. headwear/hats, shirts, etc.

---
+++ XLGearModifier/Patches/CharacterCustomizerPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+
+namespace XLGearModifier.Patches
+{
+	public class CharacterCustomizerPatch
+	{
+		[HarmonyPatch(typeof(CharacterCustomizer), nameof(CharacterCustomizer.EquipCharacterGear), new [] { typeof(CharacterGearInfo), typeof(bool) })]
+		static class EquipCharacterGearPatch
+		{
+			static bool Prefix(CharacterCustomizer __instance, CharacterGearInfo gear, bool updateMask)
+			{
+				if (!Main.Enabled || !Settings.Instance.AllowMultipleGearItemsPerSlot) return true;
+
+				Traverse traverse = Traverse.Create(__instance);
+				ClothingGearObjet shoes = traverse.Method("LoadClothingAsync", gear).GetValue<ClothingGearObjet>();
+				if (shoes == null)
+					throw new Exception("Failed to load clothing gear: " + gear);
+				shoes.SetVisible(true);
+
+				if (shoes.template.category == ClothingGearCategory.Shoes && traverse.Field("showColoredShoes").GetValue<bool>())
+					traverse.Method("MakeShoesColored", shoes, true);
+
+				traverse.Field("equippedGear").GetValue<List<ClothingGearObjet>>().Add(shoes);
+				if (!updateMask)
+					return false;
+
+				traverse.Method("UpdateMasksFrom", (IEnumerable<ClothingGearObjet>) traverse.Field("equippedGear"));
+				traverse.Method("ApplyMasks");
+
+				return false;
+			}
+		}
+	}
+}
---
+++ XLGearModifier/UserInterface.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace XLGearModifier
+{
+	public class UserInterface : MonoBehaviour
+	{
+		private static UserInterface _instance;
+		public static UserInterface Instance => _instance ?? (_instance = new UserInterface());
+
+		private bool UIIsOpen;
+
+		private void Update()
+		{
+			if (!Input.GetKeyDown(KeyCode.Backslash)) return;
+
+			UIIsOpen = !UIIsOpen;
+			Cursor.visible = UIIsOpen;
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		private void OnGUI()
+		{
+			if (!UIIsOpen) return;
+
+			GUI.backgroundColor = Color.black;
+
+			var style = new GUIStyle(GUI.skin.window)
+			{
+				contentOffset = new Vector2(0, -20),
+				stretchHeight = false,
+				stretchWidth = false
+			};
+            
+			GUILayout.Window(823, new Rect(40, 40, 200, 50), DrawWindow, "XL Gear Modifier", style);
+		}
+
+		private void DrawWindow(int windowID)
+		{
+			GUI.DragWindow(new Rect(0.0f, 0.0f, 10000f, 20f));
+			GUI.backgroundColor = Color.black;
+
+			CreateToggleButton();
+		}
+
+		private void CreateToggleButton()
+		{
+			GUIStyle style = new GUIStyle(GUI.skin.button) { fontSize = 14, fixedHeight = 25 };
+			style.normal.textColor = style.hover.textColor = style.active.textColor = Settings.Instance.AllowMultipleGearItemsPerSlot ? Color.green : Color.white;
+
+			GUILayout.BeginHorizontal();
+
+			if (GUILayout.Button(new GUIContent($"Allow Multiple Gear Items Per Slot: <b>{(Settings.Instance.AllowMultipleGearItemsPerSlot ? "Yes" : "No")}</b>"), style))
+			{
+				Settings.Instance.AllowMultipleGearItemsPerSlot = !Settings.Instance.AllowMultipleGearItemsPerSlot;
+				Settings.Instance.Save();
+			}
+			
+			GUILayout.EndHorizontal();
+		}
+	}
+}
