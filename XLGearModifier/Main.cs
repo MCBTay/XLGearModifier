85137bb91a42cad12293abc11ccb04de4aec0813
Bryan Taylor <mcbtay@gmail.com>
2020-10-15 20:58:38 -0400

    initial commit.  a quick and dirty mod that will allow you to apply multiple gear items in a single slot, i.e. headwear/hats, shirts, etc.

---
+++ XLGearModifier/Main.cs
@@ -0,0 +1,63 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace XLGearModifier
+{
+#if DEBUG
+	[EnableReloading]
+#endif
+	static class Main
+	{
+		public static bool Enabled { get; private set; }
+		private static Harmony Harmony { get; set; }
+		private static GameObject UserInterfaceGameObject { get; set; }
+
+		static bool Load(UnityModManager.ModEntry modEntry)
+		{
+			Settings.Instance = UnityModManager.ModSettings.Load<Settings>(modEntry);
+			Settings.ModEntry = modEntry;
+
+			modEntry.OnToggle = OnToggle;
+#if DEBUG
+			modEntry.OnUnload = Unload;
+#endif
+
+			return true;
+		}
+
+		private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+		{
+			if (Enabled == value) return true;
+			Enabled = value;
+
+			if (Enabled)
+			{
+				Harmony = new Harmony(modEntry.Info.Id);
+				Harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+				UserInterfaceGameObject = new GameObject();
+				UserInterfaceGameObject.AddComponent<UserInterface>();
+				Object.DontDestroyOnLoad(UserInterfaceGameObject);
+			}
+			else
+			{
+				Object.DestroyImmediate(UserInterfaceGameObject);
+				Harmony.UnpatchAll(Harmony.Id);
+			}
+
+			return true;
+		}
+
+#if DEBUG
+		static bool Unload(UnityModManager.ModEntry modEntry)
+		{
+			Object.DestroyImmediate(UserInterfaceGameObject);
+
+			Harmony?.UnpatchAll();
+			return true;
+		}
+#endif
+	}
+}
---
+++ XLGearModifier/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityModManagerNet;
+
+namespace XLGearModifier
+{
+	[Serializable]
+	public class Settings : UnityModManager.ModSettings
+	{
+		public bool AllowMultipleGearItemsPerSlot { get; set; }
+
+		public static Settings Instance { get; set; }
+		public static UnityModManager.ModEntry ModEntry;
+
+		public Settings() : base()
+		{
+			
+		}
+
+		public override void Save(UnityModManager.ModEntry modEntry)
+		{
+			Save(this, modEntry);
+		}
+
+		public void Save()
+		{
+			Save(ModEntry);
+		}
+	}
+}
