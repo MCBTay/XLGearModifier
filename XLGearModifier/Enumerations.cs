05f52d09dae84485a711e15d1084e6cf98724198
Bryan Taylor <mcbtay@gmail.com>
2020-10-24 17:00:01 -0400

    setting it so that it's only applicable to hair/headwear

---
+++ XLGearModifier/Enumerations.cs
@@ -0,0 +1,16 @@
+﻿namespace XLGearModifier
+{
+	public enum GearCategory
+	{
+		SkinTone = 0,
+		Hair = 1,
+		Headwear = 2,
+		Top = 3,
+		Bottom = 4,
+		Shoes = 5,
+		Deck = 6,
+		Griptape = 7,
+		Trucks = 8,
+		Wheels = 9
+	}
+}
