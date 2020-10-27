f44993504316bb7a58b689bd66e8420717cdf445
Bryan Taylor <mcbtay@gmail.com>
2020-10-27 08:24:25 -0400

    adding a class to load the embedded asset bundle

---
+++ XLGearModifier/AssetBundleHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace XLGearModifier
+{
+	public static class AssetBundleHelper
+	{
+		public static List<GameObject> Hair = new List<GameObject>();
+
+		public static List<CharacterGearInfo> HairGearInfo = new List<CharacterGearInfo>();
+
+		public static void LoadGearBundle()
+		{
+			AssetBundle bundle = AssetBundle.LoadFromMemory(ExtractResource("XLGearModifier.Assets.customgear"));
+
+			var prefab = bundle.LoadAsset<GameObject>("Assets/Meshes/Hair/Long-Hair-Beanie--Hair.fbx");
+			var gearPrefabController = prefab.AddComponent<GearPrefabController>();
+			Traverse.Create(gearPrefabController).Field("_skinnedMeshRenderer").SetValue(prefab.GetComponentInChildren<SkinnedMeshRenderer>());
+			gearPrefabController.PreparePrefab();
+			Hair.Add(prefab);  //based on conterpart
+
+			HairGearInfo.Add(new CharacterGearInfo(prefab.name, HairStyles.MHairCounterpart.ToString(), true, new TextureChange[0], new string[0] ));
+		}
+
+		private static byte[] ExtractResource(string filename)
+		{
+			Assembly a = Assembly.GetExecutingAssembly();
+			using (var resFilestream = a.GetManifestResourceStream(filename))
+			{
+				if (resFilestream == null) return null;
+				byte[] ba = new byte[resFilestream.Length];
+				resFilestream.Read(ba, 0, ba.Length);
+				return ba;
+			}
+		}
+	}
+}
