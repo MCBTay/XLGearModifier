c5b4af3688e49ca7d17ad0c54df81d31634b3f56
Bryan Taylor <mcbtay@gmail.com>
2020-10-27 21:59:27 -0400

    simplify the asset bundle helper to only deal with asset bundles

--- XLGearModifier/AssetBundleHelper.cs
+++ XLGearModifier/AssetBundleHelper.cs
@@ -1,27 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Reflection;
-using HarmonyLib;
 using UnityEngine;
 
 namespace XLGearModifier
 {
 	public static class AssetBundleHelper
 	{
-		public static List<GameObject> Hair = new List<GameObject>();
-
-		public static List<CharacterGearInfo> HairGearInfo = new List<CharacterGearInfo>();
-
 		public static void LoadGearBundle()
 		{
 			AssetBundle bundle = AssetBundle.LoadFromMemory(ExtractResource("XLGearModifier.Assets.customgear"));
-
-			var prefab = bundle.LoadAsset<GameObject>("Assets/Meshes/Hair/Long-Hair-Beanie--Hair.fbx");
-			var gearPrefabController = prefab.AddComponent<GearPrefabController>();
-			Traverse.Create(gearPrefabController).Field("_skinnedMeshRenderer").SetValue(prefab.GetComponentInChildren<SkinnedMeshRenderer>());
-			gearPrefabController.PreparePrefab();
-			Hair.Add(prefab);  //based on conterpart
-
-			HairGearInfo.Add(new CharacterGearInfo(prefab.name, HairStyles.MHairCounterpart.ToString(), true, new TextureChange[0], new string[0] ));
+			GearManager.Instance.LoadAssets(bundle);
 		}
 
 		private static byte[] ExtractResource(string filename)
