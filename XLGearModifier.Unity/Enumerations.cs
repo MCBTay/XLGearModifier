dff998a283942b22d6d34766de5325cf55db2822
Bryan Taylor <mcbtay@gmail.com>
2020-10-28 09:49:15 -0400

    adding a new DLL to be used in the unity project.  this will tell us what category of gear a mesh belongs in, as well as the "base" mesh we want it to behave like.

---
+++ XLGearModifier.Unity/Enumerations.cs
@@ -0,0 +1,42 @@
+﻿namespace XLGearModifier.Unity
+{
+	public enum GearCategory
+	{
+		SkinTone = 0,
+		Hair = 1,
+		Headwear = 2,
+		Top = 3,
+		Bottom = 4,
+		Shoes = 5,
+		Deck = 6,
+		Griptape = 7,
+		Trucks = 8,
+		Wheels = 9
+	}
+
+	public enum HairStyles
+	{
+		MHairAfroShort,
+		MHairAfroLong,
+		MHairCounterpart,
+		MHairCurlyLong,
+		MHairCurlyShort,
+		MHairDreads,
+		MHairPompadour,
+		MHairSidepart,
+		MHairSpikt,
+		MHairWavy,
+		FHairBraidedPonytail,
+		FHairShortPonytail,
+		FHairStraightBun,
+	}
+
+	public enum HeadwearTypes
+	{
+		MHat5Panel,
+		MHatBeanieCuffed,
+		MHatDad,
+		MHatSnapback,
+		FHatDad
+	}
+}
