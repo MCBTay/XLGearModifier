f14ff95fb59e8ef6a475847a19e050c6c0435d18
Bryan Taylor <mcbtay@gmail.com>
2020-10-27 22:22:22 -0400

    patch to pull the custom prefab, needs more work but is fine for now

---
+++ XLGearModifier/Patches/GearObjectPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace XLGearModifier.Patches
+{
+	public class GearObjectPatch
+	{
+		[HarmonyPatch(typeof(GearObject), "LoadPrefab")]
+		public static class LoadPrefabPatch
+		{
+			static void Postfix(GearObject __instance, string path, ref Task<GameObject> __result)
+			{
+				//TODO: Update this hardcoded nonsense.
+				if (__instance.gearInfo.name.StartsWith("Long-Hair-Beanie--Hair"))
+				{
+					__result = Task.FromResult(GearManager.Instance.CustomGear[GearCategory.Hair].First().Prefab);
+				}
+			}
+		}
+	}
+}
