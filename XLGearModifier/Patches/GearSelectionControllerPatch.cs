baf8b1f7fc133ec26b7ef0bc20179e375ebeb996
Bryan Taylor <mcbtay@gmail.com>
2020-10-27 22:01:17 -0400

    add patch to override GetNumberOfItems

---
+++ XLGearModifier/Patches/GearSelectionControllerPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System.Linq;
+
+namespace XLGearModifier.Patches
+{
+	public class GearSelectionControllerPatch
+	{
+		[HarmonyPatch(typeof(GearSelectionController), nameof(GearSelectionController.GetNumberOfItems))]
+		public static class GetNumberOfItemsPatch
+		{
+			static void Postfix(ref int __result, IndexPath index)
+			{
+				var gear = Traverse.Create(GearDatabase.Instance).Field("gearListSource").GetValue<GearInfo[][][]>();
+
+				if (index[0] < 0) return;
+				bool isCustom = index[0] < gear.Length && index[1] >= gear[index[0]].Length;
+				if (!isCustom) return;
+
+				var customIndex = index[1] - gear[index[0]].Length;
+				__result += GearManager.Instance.CustomGear[(GearCategory)customIndex].Count;
+			}
+		}
+	}
+}
