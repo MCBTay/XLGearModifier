be6135e740422cdf52f03d48204c81e01e12422a
Bryan Taylor <mcbtay@gmail.com>
2020-10-28 09:49:15 -0400

    adding a new DLL to be used in the unity project.  this will tell us what category of gear a mesh belongs in, as well as the "base" mesh we want it to behave like.

---
+++ XLGearModifier.Unity/XLGearModifierMetadata.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace XLGearModifier.Unity
+{
+	public class XLGearModifierMetadata : MonoBehaviour
+    {
+	    public GearCategory Category;
+
+		[HideIfEnumValue("Category", HideIf.NotEqual, (int)GearCategory.Hair)]
+	    public HairStyles BaseHairStyle;
+
+	    [HideIfEnumValue("Category", HideIf.NotEqual, (int)GearCategory.Headwear)]
+		public HeadwearTypes BaseHeadwearType;
+    }
+}
