61272cd1fd14ca9409676c1633c5c2ba6e349fec
Bryan Taylor <mcbtay@gmail.com>
2020-10-27 21:57:10 -0400

    creating a custom gear class and a gear manager singleton

---
+++ XLGearModifier/CustomGear.cs
@@ -0,0 +1,79 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace XLGearModifier
+{
+	public class CustomGear
+	{
+		public GameObject Prefab;
+		public CharacterGearInfo GearInfo;
+		public GearCategory Category;
+		public string Type;
+
+		public CustomGear(GearCategory category, string type, GameObject prefab)
+		{
+			Category = category;
+			Type = type;
+			Prefab = prefab;
+			GearInfo = new CharacterGearInfo(prefab.name, type, true, new TextureChange[0], new string[0]);
+
+			AddGearPrefabController();
+			AddMaterialController();
+		}
+
+		private void AddGearPrefabController()
+		{
+			var gearPrefabController = Prefab.AddComponent<GearPrefabController>();
+			gearPrefabController.PreparePrefab();
+		}
+
+		private async Task AddMaterialController()
+		{
+			var origMaterialController = await GetDefaultGearMaterialController(Type);
+
+			if (origMaterialController != null)
+			{
+				var newMaterialController = Prefab.AddComponent<MaterialController>();
+
+				newMaterialController.PropertyNameSubstitutions = origMaterialController.PropertyNameSubstitutions;
+
+				newMaterialController.targets = new List<MaterialController.TargetMaterialConfig>();
+				foreach (var target in origMaterialController.targets)
+				{
+					var config = new MaterialController.TargetMaterialConfig
+					{
+						renderer = Prefab.GetComponentInChildren<SkinnedMeshRenderer>(),
+						materialIndex = target.materialIndex,
+						sharedMaterial = target.sharedMaterial
+					};
+					config.renderer.sharedMaterials = target.renderer.sharedMaterials;
+
+					newMaterialController.targets.Add(config);
+				}
+
+				newMaterialController.alphaMasks = origMaterialController.alphaMasks;
+				newMaterialController.materialID = origMaterialController.materialID;
+				Traverse.Create(newMaterialController).Field("_originalMaterial").SetValue(Traverse.Create(origMaterialController).Field("originalMaterial").GetValue<Material>());
+			}
+		}
+
+		private async Task<MaterialController> GetDefaultGearMaterialController(string type)
+		{
+			var gear = Traverse.Create(GearDatabase.Instance).Field("gearListSource").GetValue<GearInfo[][][]>();
+			var officialMaleHair = gear[0][1];
+
+			var charGearInfo = officialMaleHair.Where(x => x.type == type).Cast<CharacterGearInfo>().FirstOrDefault();
+			if (charGearInfo != null)
+			{
+				var clothingGearObj = new ClothingGearObjet(charGearInfo, PlayerController.Instance.characterCustomizer);
+				var defaultGearPrefab = await Traverse.Create(clothingGearObj).Method("LoadPrefab", clothingGearObj.template.path).GetValue<Task<GameObject>>();
+				return defaultGearPrefab.GetComponentInChildren<MaterialController>();
+			}
+
+			return null;
+		}
+	}
+}
---
+++ XLGearModifier/GearManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XLGearModifier
+{
+	public class GearManager
+	{
+		private static GearManager __instance;
+		public static GearManager Instance => __instance ?? (__instance = new GearManager());
+
+		public Dictionary<GearCategory, List<CustomGear>> CustomGear;
+
+		public GearManager()
+		{
+			CustomGear = new Dictionary<GearCategory, List<CustomGear>>();
+
+			foreach (GearCategory category in Enum.GetValues(typeof(GearCategory)))
+			{
+				CustomGear.Add(category, new List<CustomGear>());
+			}
+		}
+
+		public void LoadAssets(AssetBundle bundle)
+		{
+			LoadAsset(bundle, "Assets/Meshes/Hair/Long-Hair-Beanie--Hair--000sideways.fbx", GearCategory.Hair, HairStyles.MHairCounterpart.ToString());
+		}
+
+		private void LoadAsset(AssetBundle bundle, string path, GearCategory category, string type)
+		{
+			var prefab = bundle.LoadAsset<GameObject>(path);
+			CustomGear[category].Add(new CustomGear(category, type, prefab));
+		}
+	}
+}
