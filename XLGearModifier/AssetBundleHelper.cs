4d01dfd0d23a73b77ba6605fd56060f21d62e513
Bryan Taylor <mcbtay@gmail.com>
2020-11-09 18:44:12 -0500

    adding support for custom asset packs

--- XLGearModifier/AssetBundleHelper.cs
+++ XLGearModifier/AssetBundleHelper.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
@@ -6,10 +10,61 @@ namespace XLGearModifier
 {
 	public static class AssetBundleHelper
 	{
+		public static string AssetPacksPath;
+
 		public static void LoadGearBundle()
 		{
 			AssetBundle bundle = AssetBundle.LoadFromMemory(ExtractResource("XLGearModifier.Assets.customgear"));
 			GearManager.Instance.LoadAssets(bundle);
+		}
+
+		public static IEnumerator LoadUserBundles()
+		{
+			AssetPacksPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "SkaterXL", "XLGearModifier", "Asset Packs");
+
+			if (!Directory.Exists(AssetPacksPath))
+			{
+				Directory.CreateDirectory(AssetPacksPath);
+			}
+
+			foreach (var assetPack in Directory.GetFiles(AssetPacksPath, "*", SearchOption.AllDirectories))
+			{
+				if (Path.HasExtension(assetPack)) continue;
+
+				yield return PlayerController.Instance.StartCoroutine(LoadBundleAsync(assetPack));
+			}
+		}
+
+		static IEnumerator LoadBundleAsync(string name, bool isEmbedded = false)
+		{
+			AssetBundleCreateRequest abCreateRequest;
+
+			if (isEmbedded)
+			{
+				abCreateRequest = AssetBundle.LoadFromMemoryAsync(ExtractResource(name));
+			}
+			else
+			{
+				abCreateRequest = AssetBundle.LoadFromFileAsync(name);
+			}
+
+			yield return abCreateRequest;
+
+			var bundle = abCreateRequest?.assetBundle;
+			if (bundle == null) yield break;
+
+			var assetLoadRequest = bundle.LoadAllAssetsAsync<GameObject>();
+			yield return assetLoadRequest;
+
+			var assets = assetLoadRequest.allAssets;
+			if (assets == null || !assets.Any()) yield break;
+
+			foreach (GameObject asset in assets)
+			{
+				GearManager.Instance.AddPrefab(asset);
+			}
+
+			bundle.Unload(false);
 		}
 
 		private static byte[] ExtractResource(string filename)
