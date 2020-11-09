9bb036465d1e4862ea71f084f60e3731b75a04d5
Bryan Taylor <mcbtay@gmail.com>
2020-11-09 18:32:12 -0500

    simplifying some of the code to calculate the number of items

--- XLGearModifier/Patches/GearSelectionControllerPatch.cs
+++ XLGearModifier/Patches/GearSelectionControllerPatch.cs
@@ -1,5 +1,4 @@
 ﻿using HarmonyLib;
-using System.Linq;
 using XLGearModifier.Unity;
 
 namespace XLGearModifier.Patches
@@ -13,12 +12,12 @@ namespace XLGearModifier.Patches
 			{
 				var gear = Traverse.Create(GearDatabase.Instance).Field("gearListSource").GetValue<GearInfo[][][]>();
 
-				if (index[0] < 0) return;
+				if (index[0] < 0 && index.depth <= 1) return;
 				bool isCustom = index[0] < gear.Length && index[1] >= gear[index[0]].Length;
 				if (!isCustom) return;
 
-				var customIndex = index[1] - gear[index[0]].Length;
-				__result += GearManager.Instance.CustomGear[(GearCategory)customIndex].Count;
+				if (GearManager.Instance.CustomGear.ContainsKey((GearCategory)index[1]))
+					__result += GearManager.Instance.CustomGear[(GearCategory)index[1]].Count;
 			}
 		}
 	}
