9ef747c5d85cd592824653a7aecd7f3c7e0bccab
Bryan Taylor <mcbtay@gmail.com>
2020-11-20 22:12:45 -0500

    first pass at a menu structure for custom meshes

--- XLGearModifier/Patches/GearDatabasePatch.cs
+++ XLGearModifier/Patches/GearDatabasePatch.cs
@@ -1,10 +1,32 @@
 ﻿using HarmonyLib;
+using System.Collections.Generic;
 using System.Linq;
+using XLMenuMod.Utilities;
+using XLMenuMod.Utilities.Gear;
+using XLMenuMod.Utilities.Interfaces;
 
 namespace XLGearModifier.Patches
 {
 	public class GearDatabasePatch
 	{
+		[HarmonyPatch(typeof(GearDatabase), nameof(GearDatabase.GetGearListAtIndex), new[] { typeof(IndexPath), typeof(bool) }, new[] { ArgumentType.Normal, ArgumentType.Out })]
+		public static class GetGearListAtIndexPatch
+		{
+			static void Postfix(GearDatabase __instance, IndexPath index, ref GearInfo[][][] ___gearListSource, ref GearInfo[] __result)
+			{
+				if (index.depth < 2) return;
+
+				List<ICustomInfo> sourceList = null;
+				if (index[1] == 20)
+				{
+					sourceList = GearManager.Instance.CurrentFolder.HasChildren() ? GearManager.Instance.CurrentFolder.Children : GearManager.Instance.NestedItems;
+				}
+
+				if (sourceList == null) return;
+				__result = sourceList.Select(x => x.GetParentObject() as GearInfo).ToArray();
+			}
+		}
+
 		[HarmonyPatch(typeof(GearDatabase), nameof(GearDatabase.GetGearAtIndex), new[] { typeof(IndexPath), typeof(bool) }, new[] { ArgumentType.Normal, ArgumentType.Out })]
 		public static class GetGearAtIndexPatch
 		{
@@ -12,7 +34,28 @@ namespace XLGearModifier.Patches
 			{
 				if (index.depth >= 3 && index[1] == 20)
 				{
-					__result = GearManager.Instance.CustomGear.ElementAt(index[2]).GearInfo;
+					if (index.depth == 3)
+					{
+						switch (GearManager.Instance.NestedItems.ElementAt(index.LastIndex).GetParentObject())
+						{
+							case CustomGearFolderInfo customGearFolderInfo:
+								__result = customGearFolderInfo;
+								break;
+						}
+					}
+					// mesh per type, you've already selected a type so current folder should be valid
+					else if (index.depth >= 4)
+					{
+						switch (GearManager.Instance.CurrentFolder.Children.ElementAt(index.LastIndex).GetParentObject())
+						{
+							case CustomCharacterGearInfo customCharacterGearInfo:
+								__result = customCharacterGearInfo;
+								break;
+	   						case CustomGearFolderInfo customGearFolderInfo:
+								__result = customGearFolderInfo;
+								break;
+						}
+					}
 				}
 			}
 		}
---
+++ XLGearModifier/Patches/MVCListViewPatch.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+
+namespace XLGearModifier.Patches
+{
+	static class MVCListViewPatch
+	{
+		[HarmonyPatch(typeof(MVCListView), "Header_OnPreviousCategory")]
+		public static class Header_OnPreviewCategoryPatch
+		{
+			static void Prefix(MVCListView __instance)
+			{
+				if (__instance.DataSource != null)
+				{
+					var newIndexPath = __instance.currentIndexPath;
+
+					if (__instance.DataSource is GearSelectionController && __instance.currentIndexPath.depth > 2)
+					{
+						while (newIndexPath.depth != 2) { newIndexPath = newIndexPath.Up(); }
+
+						Traverse.Create(__instance).Property("currentIndexPath").SetValue(newIndexPath);
+						GearManager.Instance.CurrentFolder = null;
+					}
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(MVCListView), "Header_OnNextCategory")]
+		public static class Header_OnNextCategoryPatch
+		{
+			static void Prefix(MVCListView __instance)
+			{
+				if (__instance.DataSource != null)
+				{
+					var newIndexPath = __instance.currentIndexPath;
+
+					if (__instance.DataSource is GearSelectionController && __instance.currentIndexPath.depth > 2)
+					{
+						while (newIndexPath.depth != 2) { newIndexPath = newIndexPath.Up(); }
+
+						Traverse.Create(__instance).Property("currentIndexPath").SetValue(newIndexPath);
+						GearManager.Instance.CurrentFolder = null;
+					}
+				}
+			}
+		}
+	}
+}
