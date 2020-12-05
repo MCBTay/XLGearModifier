f5cec0b32eee6ca29aa9be3d5fd11765eafe665f
Bryan Taylor <mcbtay@gmail.com>
2020-12-05 15:30:04 -0500

    adding some support for alpha masks to the metadata script

---
+++ XLGearModifier.Unity/AlphaMaskTextureInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace XLGearModifier.Unity
+{
+	// This class pulled from SkaterXL.  Will get mapped over in mod code to the real version of this class.
+	[Serializable]
+	public class AlphaMaskTextureInfo
+	{
+		public AlphaMaskLocation type;
+		public Texture2D texture;
+	}
+}
---
+++ XLGearModifier.Unity/GearAlphaMaskConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace XLGearModifier.Unity
+{
+	// This class pulled from SkaterXL.  Will get mapped over in mod code to the real version of this class.
+	[Serializable]
+	public class GearAlphaMaskConfig
+	{
+		public AlphaMaskLocation MaskLocation;
+		[Range(0.0f, 255f)]
+		public int Threshold;
+	}
+}
