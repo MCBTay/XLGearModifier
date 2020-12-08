ff0a6da6c1d4b0d6c3474d52a7a2613d451edd45
Bryan Taylor <mcbtay@gmail.com>
2020-12-08 00:33:46 -0500

    add a new "base" texture for any meshes that aren't based on default gear

---
+++ XLGearModifier/Patches/TextureMangingClassPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace XLGearModifier.Patches
+{
+	public class TextureMangingClassPatch
+	{
+		[HarmonyPatch(typeof(TextureMangingClass), "LoadTextureAsync")]
+		static class LoadTextureAsyncPatch
+		{
+			static void Postfix(string texturePath, bool linear, ref Task<Texture> __result)
+			{
+				if (texturePath.StartsWith("XLGearModifier"))
+				{
+					__result = Task.FromResult<Texture>(AssetBundleHelper.emptyAlbedo);
+				}
+			}
+		}
+	}
+}
