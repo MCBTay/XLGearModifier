5fb3104c68596adeb07798ae61007c6ebd624317
Bryan Taylor <mcbtay@gmail.com>
2020-12-13 15:07:12 -0500

    reworking XLGearModifierMetadata to be 3 separate scripts

---
+++ XLGearModifier.Unity/XLGMBoardGearMetadata.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace XLGearModifier.Unity
+{
+	public class XLGMBoardGearMetadata : XLGMMetadata
+	{
+		[Tooltip("This is the Category of the gear and will dictate which category folder the mesh shows up in.")]
+		public BoardGearCategory Category;
+
+		[Header("Texturing")]
+		public bool BaseOnDefaultGear;
+
+		#region Base Styles
+		[HideIf(nameof(BaseOnDefaultGear), false)]
+		[HideIfEnumValue(nameof(Category), HideIf.NotEqual, (int)BoardGearCategory.Trucks)]
+		[Tooltip("This is the list of base truck styles in the game.")]
+		public TruckTypes BaseTruckStyle;
+		#endregion
+
+		public override string GetBaseType()
+		{
+			switch (Category)
+			{
+				case BoardGearCategory.Deck: return "Deck";
+				case BoardGearCategory.Griptape: return "Griptape";
+				case BoardGearCategory.Trucks: return BaseTruckStyle.ToString();
+				case BoardGearCategory.Wheels: return "Wheels";
+				default: return string.Empty;
+			}
+		}
+
+		public override string GetSprite() => null;
+		public override string GetCategory() => Category.ToString();
+		public override bool BasedOnDefaultGear() => BaseOnDefaultGear;
+		public override XLGMTextureSet GetMaterialInformation() => null;
+	}
+}
---
+++ XLGearModifier.Unity/XLGMSkaterMetadata.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace XLGearModifier.Unity
+{
+	public class XLGMSkaterMetadata : XLGMMetadata
+	{
+		public SkaterBase BasedOn;
+
+		[Header("Materials")]
+		public List<XLGMTextureSet> TextureSets;
+
+		public override string GetBaseType() => BasedOn.ToString();
+		public override string GetSprite() => null;
+		public override string GetCategory() => null;
+		public override bool BasedOnDefaultGear() => false;
+
+		public override XLGMTextureSet GetMaterialInformation() => TextureSets?.FirstOrDefault();
+	}
+}
