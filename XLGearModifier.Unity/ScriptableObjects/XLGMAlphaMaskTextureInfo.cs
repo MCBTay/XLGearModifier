cca623db2a5b538d2368b1e14ad6dc952812d1c3
Bryan Taylor <mcbtay@gmail.com>
2020-12-13 17:28:03 -0500

    converting the alpha mask configs to scriptable objects

--- XLGearModifier.Unity/ScriptableObjects/XLGMAlphaMaskTextureInfo.cs
+++ XLGearModifier.Unity/ScriptableObjects/XLGMAlphaMaskTextureInfo.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using UnityEngine;
 
-namespace XLGearModifier.Unity
+namespace XLGearModifier.Unity.ScriptableObjects
 {
 	// This class pulled from SkaterXL.  Will get mapped over in mod code to the real version of this class.
+	[CreateAssetMenu(fileName = "New XLGM Alpha Mask Texture Info", menuName = "XLGM Alpha Mask Texture Info")]
 	[Serializable]
-	public class AlphaMaskTextureInfo
+	public class XLGMAlphaMaskTextureInfo : ScriptableObject
 	{
 		public AlphaMaskLocation type;
 		public Texture2D texture;
