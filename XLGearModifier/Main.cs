6ceb6a17dab18649458de6e42e5ca04b9b4a9118
Bryan Taylor <mcbtay@gmail.com>
2020-12-17 21:14:38 -0500

    removing the user interface class as it's no longer necessary

--- XLGearModifier/Main.cs
+++ XLGearModifier/Main.cs
@@ -1,10 +1,8 @@
-﻿using System.Linq;
-using HarmonyLib;
+﻿using HarmonyLib;
+using System.Linq;
 using System.Reflection;
-using UnityEngine;
 using UnityModManagerNet;
 using XLMenuMod.Utilities.UserInterface;
-using Object = UnityEngine.Object;
 
 namespace XLGearModifier
 {
@@ -15,7 +13,6 @@ namespace XLGearModifier
 	{
 		public static bool Enabled { get; private set; }
 		private static Harmony Harmony { get; set; }
-		private static GameObject UserInterfaceGameObject { get; set; }
 		public static bool XLMenuModEnabled { get; private set; }
 
 		static bool Load(UnityModManager.ModEntry modEntry)
@@ -41,10 +38,6 @@ namespace XLGearModifier
 				Harmony = new Harmony(modEntry.Info.Id);
 				Harmony.PatchAll(Assembly.GetExecutingAssembly());
 
-				UserInterfaceGameObject = new GameObject();
-				UserInterfaceGameObject.AddComponent<UserInterface>();
-				Object.DontDestroyOnLoad(UserInterfaceGameObject);
-
 				var xlMenuMod = UnityModManager.modEntries.FirstOrDefault(x => x.Info.Id == "XLMenuMod");
 				if (xlMenuMod != null)
 				{
@@ -59,7 +52,6 @@ namespace XLGearModifier
 			}
 			else
 			{
-				Object.DestroyImmediate(UserInterfaceGameObject);
 				Harmony.UnpatchAll(Harmony.Id);
 			}
 
@@ -69,8 +61,6 @@ namespace XLGearModifier
 #if DEBUG
 		static bool Unload(UnityModManager.ModEntry modEntry)
 		{
-			Object.DestroyImmediate(UserInterfaceGameObject);
-
 			Harmony?.UnpatchAll();
 			return true;
 		}
