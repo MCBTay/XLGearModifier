a9d92c00c99b57bffaee40578e7078f07685d707
Bryan Taylor <mcbtay@gmail.com>
2020-12-31 00:59:35 -0500

    reworking texture set to be able to include more than a single default texture

--- XLGearModifier.Unity/ScriptableObjects/XLGMTextureSet.cs
+++ XLGearModifier.Unity/ScriptableObjects/XLGMTextureSet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace XLGearModifier.Unity.ScriptableObjects
@@ -7,11 +8,9 @@ namespace XLGearModifier.Unity.ScriptableObjects
 	[Serializable]
 	public class XLGMTextureSet : ScriptableObject
 	{
-		[Tooltip("This is the default texture the shader will use unless the user supplies an override in the Gear folder.  If left blank, an empty albedo will be used.")]
-		public Texture2D textureColor;
-		[Tooltip("This is the default normal map the shader will use unless the user supplies an override in the Gear folder.  If left blank, an empty normal map will be used.")]
-		public Texture2D textureNormalMap;
-		[Tooltip("This is the default mask map the shader will use unless the user supplies an override in the Gear folder.  If left blank, an empty mask map will be used.")]
-		public Texture2D textureMaskPBR;
+		public XLGMTextureInfo DefaultTexture;
+
+		[Tooltip("Alternative textures for a mesh.")]
+		public List<XLGMTextureInfo> AlternativeTextures;
 	}
 }
