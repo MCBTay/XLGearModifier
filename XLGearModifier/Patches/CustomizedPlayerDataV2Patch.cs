c7d4078cca6e1247651c7ea41b6b8a720a4c763e
Bryan Taylor <mcbtay@gmail.com>
2021-01-03 22:57:08 -0500

    patch to aid with loading custom skaters

---
+++ XLGearModifier/Patches/CustomizedPlayerDataV2Patch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace XLGearModifier.Patches
+{
+	public class CustomizedPlayerDataV2Patch
+	{
+		[HarmonyPatch(typeof(CustomizedPlayerDataV2), nameof(CustomizedPlayerDataV2.GetBuiltInSkater))]
+		static class GetBuiltInSkaterPatch
+		{
+			static void Postfix(string skaterName, ref CustomizedPlayerDataV2 __result)
+			{
+				if (__result != null) return;
+
+				__result = new CustomizedPlayerDataV2
+				{
+					boardGear = new BoardGearInfo[] { },
+					clothingGear = new CharacterGearInfo[] { },
+					body = new CharacterBodyInfo(skaterName, "mspiderman", false, new List<MaterialChange>
+					{
+						new MaterialChange("head", new[] { new TextureChange("head", "XLGearModifier") }),
+						new MaterialChange("body", new[] { new TextureChange("body", "XLGearModifier") }),
+					}, new string[] { })
+				};
+			}
+		}
+	}
+}
