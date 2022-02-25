e168cad9ea05037fdbff65109eecfa7167c3df64
mcbtay <mcbtay@gmail.com>
2022-02-24 21:54:33 -0500

    removing unncessary usings

--- XLGearModifier/Patches/GearSelectionStatePatch.cs
+++ XLGearModifier/Patches/GearSelectionStatePatch.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GameManagement;
+﻿using GameManagement;
 using HarmonyLib;
 
 namespace XLGearModifier.Patches
 {
-	public class GearSelectionStatePatch
+    public class GearSelectionStatePatch
 	{
 		[HarmonyPatch(typeof(GearSelectionState), nameof(GearSelectionState.OnEnter))]
 		static class OnEnterPatch
