5f6d3aea6635ab8684ec85e0a74071eca66fafa6
mcbtay <mcbtay@gmail.com>
2022-03-08 19:54:52 -0500

    adding some logging messages for gear obj/prefab we're loading

--- XLGearModifier/Patches/GearObjectPatch.cs
+++ XLGearModifier/Patches/GearObjectPatch.cs
@@ -2,16 +2,28 @@
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityModManagerNet;
 
 namespace XLGearModifier.Patches
 {
     public class GearObjectPatch
 	{
+        [HarmonyPatch(typeof(CharacterGearObject), "LoadTask")]
+        public static class LoadTaskPatch
+        {
+            static void Prefix(CharacterGearObject __instance)
+            {
+                UnityModManager.Logger.Log("XLGM: Loading char gear obj for " + __instance.template.path);
+            }
+        }
+
 		[HarmonyPatch(typeof(GearObject), "LoadPrefab")]
 		public static class LoadPrefabPatch
 		{
 			static bool Prefix(GearObject __instance, string path, ref Task<GameObject> __result)
 			{
+				UnityModManager.Logger.Log("XLGM: Loading prefab for " + path);
+
 				if (!path.StartsWith("XLGearModifier")) return true;
 
 				var customGear = GearManager.Instance.CustomGear.FirstOrDefault(x => x.GearInfo != null && x.GearInfo.type == __instance.gearInfo.type);
