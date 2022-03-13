00ea969d887a0864a76347c8ef15907b1fc2e57f
mcbtay <mcbtay@gmail.com>
2022-03-13 11:41:23 -0400

    first pass of EyeTextureManager, which currently looks in gear folder for eye textures and adds them to GearManager's Eye texture list

---
+++ XLGearModifier/Texturing/EyeTextureManager.cs
@@ -0,0 +1,54 @@
+﻿using SkaterXL.Data;
+using System.IO;
+using System.Linq;
+using XLMenuMod.Utilities;
+using XLMenuMod.Utilities.Gear;
+
+namespace XLGearModifier.Texturing
+{
+    public  class EyeTextureManager
+    {
+        private static EyeTextureManager __instance;
+        public static EyeTextureManager Instance => __instance ?? (__instance = new EyeTextureManager());
+
+
+        public void LookForEyeTextures()
+        {
+            var eyeTextures = Directory.GetFiles(SaveManager.Instance.CustomGearDir, "Eyes_*.png", SearchOption.AllDirectories);
+            if (!eyeTextures.Any()) return;
+
+            CustomFolderInfo parent = null;
+
+            foreach (var eyeTexture in eyeTextures)
+            {
+                var gearInfo = new CustomCharacterGearInfo(eyeTexture, "Eyes", true, new [] { new TextureChange("eyes", eyeTexture) }, new string[] { });
+                GearManager.Instance.AddItem(gearInfo, GearManager.Instance.Eyes, ref parent);
+            }
+
+
+            //if (string.IsNullOrEmpty(texture)) return;
+            //if (!File.Exists(texture)) return;
+
+            //switch (textureFilename)
+            //{
+            //    case ColorTextureFilename:
+            //        SkateshopTexture = new Texture2D(2, 2);
+            //        if (SkateshopTexture.LoadImage(File.ReadAllBytes(texture))) return;
+            //        SkateshopTexture = null;
+            //        break;
+            //    case NormalTextureFilename:
+            //        SkateshopNormal = new Texture2D(2, 2);
+            //        if (SkateshopNormal.LoadImage(File.ReadAllBytes(texture))) return;
+            //        SkateshopNormal = null;
+            //        break;
+            //    case RgmtaoTextureFilename:
+            //        SkateshopRgMtAo = new Texture2D(2, 2);
+            //        if (SkateshopRgMtAo.LoadImage(File.ReadAllBytes(texture))) return;
+            //        SkateshopRgMtAo = null;
+            //        break;
+            //    default:
+            //        break;
+            //}
+        }
+    }
+}
