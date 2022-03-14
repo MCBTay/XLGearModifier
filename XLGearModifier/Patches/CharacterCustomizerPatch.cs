465d417c11a9cfcc0c9c9c8c4070937d266c0d2a
mcbtay <mcbtay@gmail.com>
2022-03-13 20:20:57 -0400

    adding patch for both LoadClothingAsync and PreviewItem to support eye textures

--- XLGearModifier/Patches/CharacterCustomizerPatch.cs
+++ XLGearModifier/Patches/CharacterCustomizerPatch.cs
@@ -1,6 +1,9 @@
 ﻿using HarmonyLib;
 using SkaterXL.Data;
+using SkaterXL.Gear;
+using System.Collections.Generic;
 using XLGearModifier.CustomGear;
+using XLGearModifier.Texturing;
 using XLMenuMod.Utilities;
 using XLMenuMod.Utilities.Gear;
 using XLMenuMod.Utilities.Gear.Interfaces;
@@ -26,5 +29,47 @@ namespace XLGearModifier.Patches
 				return true;
 			}
 		}
-	}
+
+        /// <summary>
+        /// Patching into CharacterCustomizer.LoadClothingAsync to support loading of eye textures.
+        /// </summary>
+        [HarmonyPatch(typeof(CharacterCustomizer), "LoadClothingAsync")]
+        static class LoadClothingAsyncPatch
+        {
+            static void Postfix(CharacterCustomizer __instance, ref CharacterGearInfo gear, ref ClothingGearObjet __result)
+            {
+                if (gear.type != "eyes" && __result != null) return;
+
+                var clothingGearObject = new ClothingGearObjet(gear, __instance);
+
+                clothingGearObject.template = new CharacterGearTemplate
+                {
+                    alphaMasks = new List<GearAlphaMaskConfig>(),
+                    id = "eyes",
+                    path = string.Empty
+                };
+
+				clothingGearObject.LoadOn();
+
+                var gearCache = Traverse.Create(__instance).Field("gearCache").GetValue<Dictionary<int, GearObject>>();
+                gearCache.Add(gear.GetHashCode(), clothingGearObject);
+				__result = clothingGearObject;
+            }
+        }
+
+        /// <summary>
+        /// Patching into CharacterCustomizer.PreviewItem in order to be able to facilitate previewing eye textures.
+        /// </summary>
+        [HarmonyPatch(typeof(CharacterCustomizer), nameof(CharacterCustomizer.PreviewItem))]
+        static class PreviewItemPatch
+        {
+            static bool Prefix(CharacterCustomizer __instance, ref GearInfo preview)
+            {
+                if (preview.type != "eyes") return true;
+                
+                EyeTextureManager.Instance.SetEyeTextures(preview as CharacterGearInfo);
+                return false;
+            }
+        }
+    }
 }
