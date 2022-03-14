5107bf83e0411a5c71007637a4aa0fda754ea7c3
mcbtay <mcbtay@gmail.com>
2022-03-14 08:35:29 -0400

    second pass at eye texture manager, setting consts for the texture names, fleshing out methods to load the textures, properly set up material controllers, and  set the eye textures

--- XLGearModifier/Texturing/EyeTextureManager.cs
+++ XLGearModifier/Texturing/EyeTextureManager.cs
@@ -1,7 +1,11 @@
-﻿using SkaterXL.Data;
+﻿using HarmonyLib;
+using SkaterXL.Data;
+using SkaterXL.Gear;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using XLMenuMod.Utilities;
+using UnityEngine;
 using XLMenuMod.Utilities.Gear;
 
 namespace XLGearModifier.Texturing
@@ -11,44 +15,105 @@ namespace XLGearModifier.Texturing
         private static EyeTextureManager __instance;
         public static EyeTextureManager Instance => __instance ?? (__instance = new EyeTextureManager());
 
+        private Texture OriginalEyeTexture;
+
+        private const string ColorTextureName = "Texture2D_4128E5C7";
+        private const string NormalTextureName = "Texture2D_BEC07F52";
+        private const string RgmtaoTextureName = "Texture2D_B56F9766";
+
+        private List<MaterialController> PreviewEyeMaterialControllers;
+
+        public EyeTextureManager()
+        {
+            PreviewEyeMaterialControllers = new List<MaterialController>();
+        }
 
         public void LookForEyeTextures()
         {
-            var eyeTextures = Directory.GetFiles(SaveManager.Instance.CustomGearDir, "Eyes_*.png", SearchOption.AllDirectories);
+            var eyeTextures = Directory.GetFiles(SaveManager.Instance.CustomGearDir, "Eyes_*.png", SearchOption.AllDirectories).ToList();
             if (!eyeTextures.Any()) return;
 
-            CustomFolderInfo parent = null;
+            GearManager.Instance.Eyes.Clear();
 
-            foreach (var eyeTexture in eyeTextures)
+            var gearInfos = GearDatabase.GetGearIn(eyeTextures, true);
+
+            foreach (var gearInfo in gearInfos)
             {
-                var gearInfo = new CustomCharacterGearInfo(eyeTexture, "Eyes", true, new [] { new TextureChange("eyes", eyeTexture) }, new string[] { });
-                GearManager.Instance.AddItem(gearInfo, GearManager.Instance.Eyes, ref parent);
+                var characterGearInfo = gearInfo as CharacterGearInfo;
+                if (characterGearInfo == null) continue;
+
+                var newGearInfo = new CustomCharacterGearInfo(gearInfo.name, gearInfo.type, gearInfo.isCustom, characterGearInfo.textureChanges, gearInfo.tags);
+                GearManager.Instance.Eyes.Add(newGearInfo.Info);
+            }
+        }
+
+        private void SetupMaterialControllers()
+        {
+            var traverse = Traverse.Create(GearSelectionController.Instance.previewCustomizer);
+
+            var currentBody = traverse.Field("currentBody").GetValue<CharacterBodyObject>();
+
+            var eyeRenderers = currentBody.gameObject.GetComponentsInChildren<SkinnedMeshRenderer>(true).Where(x => x.name.Contains("eye_mesh"));
+            foreach (var eyeRenderer in eyeRenderers)
+            {
+                if (OriginalEyeTexture == null)
+                {
+                    OriginalEyeTexture = eyeRenderer.material.GetTexture(ColorTextureName);
+                }
+
+                var materialController = eyeRenderer.gameObject.GetComponent<MaterialController>();
+                if (materialController == null)
+                {
+                    materialController = eyeRenderer.gameObject.AddComponent<MaterialController>();
+                }
+
+                materialController.alphaMasks = Array.Empty<AlphaMaskTextureInfo>();
+
+                var materialControllerTraverse = Traverse.Create(materialController);
+                materialControllerTraverse.Field("m_propertyNameSubstitutions").SetValue(new List<PropertyNameSubstitution>());
+
+                if (!materialController.PropertyNameSubstitutions.ContainsKey("albedo"))
+                {
+                    materialController.PropertyNameSubstitutions.Add("albedo", ColorTextureName);
+                }
+
+                if (!materialController.PropertyNameSubstitutions.ContainsKey("normal"))
+                {
+                    materialController.PropertyNameSubstitutions.Add("normal", NormalTextureName);
+                }
+
+                if (!materialController.PropertyNameSubstitutions.ContainsKey("rgmtao"))
+                {
+                    materialController.PropertyNameSubstitutions.Add("rgmtao", RgmtaoTextureName);
+                }
+
+                materialController.FindTargets();
+                PreviewEyeMaterialControllers.Add(materialController);
+            }
+        }
+
+        /// <summary>
+        /// Applies textures passed in via gearInfo to the passed in MeshRenderer.
+        /// </summary>
+        /// <param name="renderer">The renderer to apply the textures to.</param>
+        public void SetEyeTextures(CharacterGearInfo characterGearInfo)
+        {
+            SetupMaterialControllers();
+
+            var dict = new Dictionary<string, Texture>();
+
+            foreach (var textureChange in characterGearInfo.textureChanges)
+            {
+                var texture = new Texture2D(2, 2);
+                if (!texture.LoadImage(File.ReadAllBytes(textureChange.texturePath))) continue;
+
+                dict.Add(textureChange.textureID, texture);
             }
 
-
-            //if (string.IsNullOrEmpty(texture)) return;
-            //if (!File.Exists(texture)) return;
-
-            //switch (textureFilename)
-            //{
-            //    case ColorTextureFilename:
-            //        SkateshopTexture = new Texture2D(2, 2);
-            //        if (SkateshopTexture.LoadImage(File.ReadAllBytes(texture))) return;
-            //        SkateshopTexture = null;
-            //        break;
-            //    case NormalTextureFilename:
-            //        SkateshopNormal = new Texture2D(2, 2);
-            //        if (SkateshopNormal.LoadImage(File.ReadAllBytes(texture))) return;
-            //        SkateshopNormal = null;
-            //        break;
-            //    case RgmtaoTextureFilename:
-            //        SkateshopRgMtAo = new Texture2D(2, 2);
-            //        if (SkateshopRgMtAo.LoadImage(File.ReadAllBytes(texture))) return;
-            //        SkateshopRgMtAo = null;
-            //        break;
-            //    default:
-            //        break;
-            //}
+            foreach (var materialController in PreviewEyeMaterialControllers)
+            {
+                materialController.SetMaterial(materialController.GenerateMaterialWithChanges(dict));
+            }
         }
     }
 }
