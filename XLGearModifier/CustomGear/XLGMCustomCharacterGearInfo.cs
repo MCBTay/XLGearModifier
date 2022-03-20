d6bae749c70ba7c7ded36afc99b57a6ab1a8bbfc
mcbtay <mcbtay@gmail.com>
2022-03-20 16:53:53 -0400

    adding XLGMCustomCharacterGear who's sole purpose is to get the blendshape data into the player.json file

---
+++ XLGearModifier/CustomGear/XLGMCustomCharacterGearInfo.cs
@@ -0,0 +1,17 @@
+﻿using SkaterXL.Data;
+using System.Collections.Generic;
+using XLGearModifier.Unity;
+using XLMenuMod.Utilities.Gear;
+
+namespace XLGearModifier.CustomGear
+{
+    public class XLGMCustomCharacterGearInfo : CustomCharacterGearInfo
+    {
+        public List<XLGMBlendShapeData> blendShapes;
+
+        public XLGMCustomCharacterGearInfo(string name, string type, bool isCustom, TextureChange[] textureChanges, string[] tags) : base(name, type, isCustom, textureChanges, tags)
+        {
+
+        }
+    }
+}
