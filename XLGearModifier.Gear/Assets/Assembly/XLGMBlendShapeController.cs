a93ecd8108b972524a6f5f4ef8dd47865255aa75
mcbtay <mcbtay@gmail.com>
2022-03-20 16:11:41 -0400

    adding XLGMBlendShapeController, a script to gather blend shape data from your skinned mesh renderer to make mod handling of it a bit easier to deal with

---
+++ XLGearModifier.Gear/Assets/Assembly/XLGMBlendShapeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using XLGearModifier.Unity.Attributes;
+
+namespace XLGearModifier.Unity
+{
+    [Serializable]
+    public class XLGMBlendShapeController : MonoBehaviour
+    {
+        [ReadOnly] 
+        public SkinnedMeshRenderer SkinnedMeshRenderer;
+
+        [ReadOnly] 
+        public Mesh SkinnedMesh;
+
+        [ReadOnly] 
+        public List<XLGMBlendShapeData> BlendShapes;
+
+        public void Start()
+        {
+            BlendShapes = new List<XLGMBlendShapeData>();
+        }
+
+        public void GetBlendShapeData(GameObject gameObject)
+        {
+            if (gameObject == null) return;
+
+            SkinnedMeshRenderer = gameObject.GetComponentInChildren<SkinnedMeshRenderer>();
+            if (SkinnedMeshRenderer == null) return;
+
+            SkinnedMesh = SkinnedMeshRenderer.sharedMesh;
+            if (SkinnedMesh == null) return;
+
+            BlendShapes.Clear();
+
+            for (int i = 0; i < SkinnedMesh.blendShapeCount; i++)
+            {
+                BlendShapes.Add(new XLGMBlendShapeData
+                {
+                    index = i,
+                    name = SkinnedMesh.GetBlendShapeName(i),
+                    weight = SkinnedMeshRenderer.GetBlendShapeWeight(i)
+                });
+            }
+        }
+
+        public void ClearBlendShapeData()
+        {
+            SkinnedMeshRenderer = null;
+            SkinnedMesh = null;
+            BlendShapes.Clear();
+        }
+    }
+}
