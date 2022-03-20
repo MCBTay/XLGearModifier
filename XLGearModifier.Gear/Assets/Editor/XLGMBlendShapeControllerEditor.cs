bf8a5010f1086b5b1fbf948d7ac4e41d0766ffab
mcbtay <mcbtay@gmail.com>
2022-03-20 16:14:32 -0400

    adding custom editor for XLGMBlendShapeController with 2 buttons

---
+++ XLGearModifier.Gear/Assets/Editor/XLGMBlendShapeControllerEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+using XLGearModifier.Unity;
+
+namespace XLGearModifier.Assets.Editor
+{
+    [CustomEditor(typeof(XLGMBlendShapeController))]
+    public class XLGMBlendShapeControllerEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var item = target as XLGMBlendShapeController;
+
+            if (GUILayout.Button("Get BlendShape Data"))
+            {
+                item.GetBlendShapeData(item.gameObject);
+            }
+
+            if (GUILayout.Button("Clear BlendShape Data"))
+            {
+                item.ClearBlendShapeData();
+            }
+
+            this.DrawDefaultInspector();
+        }
+    }
+}
