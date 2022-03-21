4dc6f22703e63cab43a9ec36a2e48f7d1be6e9e0
mcbtay <mcbtay@gmail.com>
2022-03-20 21:22:40 -0400

    creating XLGMCustomizedPlayerData

---
+++ XLGearModifier/CustomGear/XLGMCustomizedPlayerData.cs
@@ -0,0 +1,14 @@
+﻿using SkaterXL.Data;
+using XLGearModifier.Patches;
+
+namespace XLGearModifier.CustomGear
+{
+    /// <summary>
+    /// This class exists solely for the <see cref="CustomizedPlayerDataV2Patch.FromStringPatch"/>, such that we can deserialize the player.json file
+    /// using our customized <see cref="XLGMCustomCharacterGearInfo"/>.
+    /// </summary>
+    public class XLGMCustomizedPlayerData : CustomizedPlayerDataV2
+    {
+        public new XLGMCustomCharacterGearInfo[] clothingGear { get; set; }
+    }
+}
