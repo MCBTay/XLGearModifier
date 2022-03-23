6ee0b55b7547a18aebe79e8b0cf910c1c81789e0
mcbtay <mcbtay@gmail.com>
2022-03-23 08:39:14 -0400

    removing prefix as it's no longer needed

--- XLGearModifier.Gear/Assets/Assembly/XLGMMetadata.cs
+++ XLGearModifier.Gear/Assets/Assembly/XLGMMetadata.cs
@@ -7,10 +7,8 @@ namespace XLGearModifier.Unity
 		[Header("Menu")]
 		[Tooltip("This is the name that will be displayed in the menu.  If left blank, we will use the prefab name.")]
 		public string DisplayName;
-		[Tooltip("This is the 'prefix' or 'type' associated with the mesh. This will be used in order for the mod to know which mesh to load but also to know which textures to load.  Arguably more important for meshes that are not based on default gear.  Examples from the base game would be MHatDad or FHatDad.")]
-		public string Prefix;
 
-		public abstract string GetBaseType();
+        public abstract string GetBaseType();
 		public abstract string GetSprite();
 		public abstract string GetCategory();
 		public abstract bool BasedOnDefaultGear();
