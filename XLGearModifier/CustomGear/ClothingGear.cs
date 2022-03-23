cca948236c210680af2fa7b3ecda88a4315ec869
mcbtay <mcbtay@gmail.com>
2022-03-23 09:16:22 -0400

    update to pull the template off of the prefab and add it directly, as it should be completely set up in editor

--- XLGearModifier/CustomGear/ClothingGear.cs
+++ XLGearModifier/CustomGear/ClothingGear.cs
@@ -222,18 +222,21 @@ namespace XLGearModifier.CustomGear
         /// </summary>
         private void AddGearTemplates()
         {
-            AddGearTemplate(ClothingMetadata.CharacterGearTemplate.id);
-            AddGearTemplate(ClothingMetadata.PrefixAlias, true);
+            var templateId = ClothingMetadata.CharacterGearTemplate.id.ToLower();
+            if (!GearDatabase.Instance.ContainsClothingTemplateWithID(templateId))
+            {
+                GearDatabase.Instance.CharGearTemplateForID.Add(templateId, ClothingMetadata.CharacterGearTemplate);
+            }
+
+            AddAliasGearTemplate(ClothingMetadata.PrefixAlias);
         }
 
-        private void AddGearTemplate(string templateId, bool isAlias = false)
+        private void AddAliasGearTemplate(string templateId)
         {
             if (string.IsNullOrEmpty(templateId)) return;
             if (GearDatabase.Instance.ContainsClothingTemplateWithID(templateId)) return;
 
-            var path = "XLGearModifier";
-            if (isAlias) path += "/alias";
-            path += $"/{templateId.ToLower()}";
+            var path = $"XLGearModifier/alias/{templateId.ToLower()}";
 
             var template = new CharacterGearTemplate
             {
@@ -242,8 +245,6 @@ namespace XLGearModifier.CustomGear
                 id = templateId.ToLower(),
                 path = path
             };
-
-            if (!isAlias) AddOrUpdateTemplateAlphaMasks(ClothingMetadata, template);
 
             GearDatabase.Instance.CharGearTemplateForID.Add(templateId.ToLower(), template);
         }
@@ -264,28 +265,6 @@ namespace XLGearModifier.CustomGear
                 default:
                 case Unity.ClothingGearCategory.Top:
                     return ClothingGearCategory.Shirt;
-            }
-        }
-
-        private void AddOrUpdateTemplateAlphaMasks(XLGMClothingGearMetadata metadata, CharacterGearTemplate template)
-        {
-            if (metadata.CharacterGearTemplate.alphaMasks == null || !metadata.CharacterGearTemplate.alphaMasks.Any()) return;
-
-            foreach (var mask in metadata.CharacterGearTemplate.alphaMasks)
-            {
-                if (mask == null) continue;
-
-                if (mask.Threshold > 250) mask.Threshold = 250;
-
-                var existing = template.alphaMasks.FirstOrDefault(x => (int)x.MaskLocation == (int)mask.MaskLocation);
-                if (existing == null)
-                {
-                    template.alphaMasks.Add(mask);
-                }
-                else
-                {
-                    existing.Threshold = mask.Threshold;
-                }
             }
         }
         #endregion
