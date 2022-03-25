08ddb23f33f4a5e5ce2c538c06216b3647224dde
mcbtay <mcbtay@gmail.com>
2022-03-25 18:45:58 -0400

    adding logic for "loading" the eyes prefab

--- XLGearModifier/Patches/GearObjectPatch.cs
+++ XLGearModifier/Patches/GearObjectPatch.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using UnityEngine;
 using XLGearModifier.CustomGear;
+using XLGearModifier.Texturing;
 
 namespace XLGearModifier.Patches
 {
@@ -13,6 +14,15 @@ namespace XLGearModifier.Patches
 			static bool Prefix(GearObject __instance, string path, ref Task<GameObject> __result)
 			{
                 if (!path.StartsWith("XLGearModifier")) return true;
+
+                if (__instance.gearInfo.type == "eyes")
+                {
+                    var customizer = Traverse.Create(__instance).Field("customizer").GetValue<CharacterCustomizer>();
+
+                    EyeTextureManager.Instance.GetGameObjectReference(customizer);
+                    __result = Task.FromResult(EyeTextureManager.Instance.EyesGameObject);
+                    return false;
+                }
 
                 var customGear = GearManager.Instance.CustomGear[__instance.gearInfo.type];
                 if (customGear == null) return true;
