a653d56152006d1df460fe2cdb3b3dba0a2fc861
mcbtay <mcbtay@gmail.com>
2022-04-01 23:47:05 -0400

    adding Template header and OnValidate to ensure id is lowercased and path is set correctly

--- XLGearModifier.Gear/Assets/Assembly/XLGMSkaterMetadata.cs
+++ XLGearModifier.Gear/Assets/Assembly/XLGMSkaterMetadata.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SkaterXL.Gear;
+using UnityEngine;
 
 namespace XLGearModifier.Unity
 {
@@ -8,6 +9,7 @@ namespace XLGearModifier.Unity
 	{
 		public SkaterBase BasedOn;
 
+		[Header("Template")]
         public CharacterBodyTemplate CharacterBodyTemplate;
 
 		public override string GetBaseType() => BasedOn.ToString();
@@ -15,5 +17,17 @@ namespace XLGearModifier.Unity
 		public override string GetCategory() => null;
 		public override bool BasedOnDefaultGear() => false;
         public override string GetTemplateId() => CharacterBodyTemplate.id.ToLower();
+
+        /// <summary>
+        /// An editor only function that gets called when this changes in inspector.  This allows us
+        /// to hide path and eye position settings.
+        /// </summary>
+        public void OnValidate()
+        {
+            if (CharacterBodyTemplate == null) return;
+
+            CharacterBodyTemplate.id = CharacterBodyTemplate.id.ToLower();
+            CharacterBodyTemplate.path = $"XLGearModifier/{CharacterBodyTemplate.id}";
+        }
     }
 }
