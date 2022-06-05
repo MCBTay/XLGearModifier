0c2a85968dbe1a87ac6cb2b7dffb716c298257ae
mcbtay <mcbtay@gmail.com>
2022-06-05 14:33:15 -0400

    custom build script for building just asset bundles

---
+++ XLGearModifier.Gear/Assets/Editor/AssetBundleUtility.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Editor
+{
+    public class AssetBundleUtility
+    {
+        public static void BuildFor(
+          GameObject prefab,
+          string outputDirectory,
+          string bundleName = null,
+          BuildAssetBundleOptions buildOptions = BuildAssetBundleOptions.ChunkBasedCompression)
+        {
+            string nearestInstanceRoot = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot((Object)prefab);
+            if (nearestInstanceRoot == null)
+                Debug.LogError((object)"Prefab not found");
+            else
+                AssetBundleUtility.BuildFor(nearestInstanceRoot, outputDirectory, bundleName ?? prefab.name, buildOptions);
+        }
+
+        public static void BuildFor(
+          string prefabAssetPath,
+          string outputDirectory,
+          string bundleName = null,
+          BuildAssetBundleOptions buildOptions = BuildAssetBundleOptions.ChunkBasedCompression)
+        {
+            AssetBundleBuild abBuild = new AssetBundleBuild()
+            {
+                assetBundleName = bundleName ?? Path.GetFileNameWithoutExtension(prefabAssetPath),
+                assetNames = new string[1] { prefabAssetPath },
+                addressableNames = new string[1] { "prefab" }
+            };
+            outputDirectory = Path.Combine(outputDirectory, EditorUserBuildSettings.activeBuildTarget.ToString());
+            AssetBundleUtility.Build(outputDirectory, abBuild, EditorUserBuildSettings.activeBuildTarget, buildOptions);
+        }
+
+        public static void BuildFor(
+          Scene scene,
+          string outputDirectory,
+          string bundleName = null,
+          BuildAssetBundleOptions buildOptions = BuildAssetBundleOptions.ChunkBasedCompression)
+        {
+            EditorSceneManager.SaveScene(scene);
+            AssetBundleBuild abBuild = new AssetBundleBuild()
+            {
+                assetBundleName = bundleName ?? scene.name,
+                assetNames = new string[1] { scene.path }
+            };
+            outputDirectory = Path.Combine(outputDirectory, EditorUserBuildSettings.activeBuildTarget.ToString());
+            AssetBundleUtility.Build(outputDirectory, abBuild, EditorUserBuildSettings.activeBuildTarget, buildOptions);
+            EditorSceneManager.OpenScene(scene.path);
+        }
+
+        public static AssetBundleManifest Build(
+          string path,
+          AssetBundleBuild abBuild,
+          BuildTarget buildTarget,
+          BuildAssetBundleOptions buildOptions)
+        {
+            return Build(path, new AssetBundleBuild[1]
+            {
+                abBuild
+            }, buildTarget, buildOptions);
+        }
+
+        public static AssetBundleManifest Build(
+          string path,
+          AssetBundleBuild[] abBuilds,
+          BuildTarget buildTarget,
+          BuildAssetBundleOptions buildOptions)
+        {
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            foreach (AssetBundleBuild abBuild in abBuilds)
+            {
+                string str1 = Path.Combine(path, abBuild.assetBundleName);
+                if (File.Exists(str1))
+                {
+                    string str2 = str1 + "_Old_" + new FileInfo(str1).CreationTime.ToString("M/d/yyyy-H:mm-tt");
+                    if (File.Exists(str2))
+                        File.Delete(str2);
+                    File.Move(str1, str2);
+                }
+            }
+            return BuildPipeline.BuildAssetBundles(path, abBuilds, buildOptions, buildTarget);
+        }
+    }
+
+}
---
+++ XLGearModifier.Gear/Assets/Editor/CIBuildScript.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+namespace Assets.Editor
+{
+    public static class CIBuildScript
+    {
+        private static readonly string Eol = Environment.NewLine;
+
+        public static void Build()
+        {
+            // Gather values from args
+            Dictionary<string, string> options = GetValidatedOptions();
+
+            // Set version for this build
+            PlayerSettings.bundleVersion = options["buildVersion"];
+
+            // Apply build target
+            var buildTarget = (BuildTarget)Enum.Parse(typeof(BuildTarget), options["buildTarget"]);
+
+            // Custom build
+            Build(buildTarget, options["customBuildPath"]);
+        }
+
+        private static Dictionary<string, string> GetValidatedOptions()
+        {
+            ParseCommandLineArguments(out Dictionary<string, string> validatedOptions);
+
+            if (!validatedOptions.TryGetValue("projectPath", out string _))
+            {
+                Console.WriteLine("Missing argument -projectPath");
+                EditorApplication.Exit(110);
+            }
+
+            if (!validatedOptions.TryGetValue("buildTarget", out string buildTarget))
+            {
+                Console.WriteLine("Missing argument -buildTarget");
+                EditorApplication.Exit(120);
+            }
+
+            if (!Enum.IsDefined(typeof(BuildTarget), buildTarget ?? string.Empty))
+            {
+                EditorApplication.Exit(121);
+            }
+
+            if (!validatedOptions.TryGetValue("customBuildPath", out string _))
+            {
+                Console.WriteLine("Missing argument -customBuildPath");
+                EditorApplication.Exit(130);
+            }
+
+            const string defaultCustomBuildName = "TestBuild";
+            if (!validatedOptions.TryGetValue("customBuildName", out string customBuildName))
+            {
+                Console.WriteLine($"Missing argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+                validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            }
+            else if (customBuildName == "")
+            {
+                Console.WriteLine($"Invalid argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+                validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            }
+
+            return validatedOptions;
+        }
+
+        private static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments)
+        {
+            providedArguments = new Dictionary<string, string>();
+            string[] args = Environment.GetCommandLineArgs();
+
+            Console.WriteLine(
+                $"{Eol}" +
+                $"###########################{Eol}" +
+                $"#    Parsing settings     #{Eol}" +
+                $"###########################{Eol}" +
+                $"{Eol}"
+            );
+
+            // Extract flags with optional values
+            for (int current = 0, next = 1; current < args.Length; current++, next++)
+            {
+                // Parse flag
+                bool isFlag = args[current].StartsWith("-");
+                if (!isFlag) continue;
+                string flag = args[current].TrimStart('-');
+
+                // Parse optional value
+                bool flagHasValue = next < args.Length && !args[next].StartsWith("-");
+                string value = flagHasValue ? args[next].TrimStart('-') : "";
+                bool secret = Secrets.Contains(flag);
+                string displayValue = secret ? "*HIDDEN*" : "\"" + value + "\"";
+
+                // Assign
+                Console.WriteLine($"Found flag \"{flag}\" with value {displayValue}.");
+                providedArguments.Add(flag, value);
+            }
+        }
+
+        private static void Build(BuildTarget buildTarget, string filePath)
+        {
+            BuildPipeline.BuildAssetBundles(filePath, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows);
+
+            //string[] scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(s => s.path).ToArray();
+            //var buildPlayerOptions = new BuildPlayerOptions
+            //{
+            //    scenes = scenes,
+            //    target = buildTarget,
+            //    //                targetGroup = BuildPipeline.GetBuildTargetGroup(buildTarget),
+            //    locationPathName = filePath,
+            //    //                options = UnityEditor.BuildOptions.Development
+            //};
+
+            //BuildSummary buildSummary = BuildPipeline.BuildPlayer(buildPlayerOptions).summary;
+            //ReportSummary(buildSummary);
+            //ExitWithResult(buildSummary.result);
+        }
+
+        private static void ReportSummary(BuildSummary summary)
+        {
+            Console.WriteLine(
+                $"{Eol}" +
+                $"###########################{Eol}" +
+                $"#      Build results      #{Eol}" +
+                $"###########################{Eol}" +
+                $"{Eol}" +
+                $"Duration: {summary.totalTime}{Eol}" +
+                $"Warnings: {summary.totalWarnings}{Eol}" +
+                $"Errors: {summary.totalErrors}{Eol}" +
+                $"Size: {summary.totalSize} bytes{Eol}" +
+                $"{Eol}"
+            );
+        }
+
+        private static void ExitWithResult(BuildResult result)
+        {
+            switch (result)
+            {
+                case BuildResult.Succeeded:
+                    Console.WriteLine("Build succeeded!");
+                    EditorApplication.Exit(0);
+                    break;
+                case BuildResult.Failed:
+                    Console.WriteLine("Build failed!");
+                    EditorApplication.Exit(101);
+                    break;
+                case BuildResult.Cancelled:
+                    Console.WriteLine("Build cancelled!");
+                    EditorApplication.Exit(102);
+                    break;
+                case BuildResult.Unknown:
+                default:
+                    Console.WriteLine("Build result is unknown!");
+                    EditorApplication.Exit(103);
+                    break;
+            }
+        }
+    }
+}
