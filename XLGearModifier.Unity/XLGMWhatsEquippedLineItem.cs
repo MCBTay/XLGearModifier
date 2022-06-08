1b3a7d977e0a4764e0924a5df85d343e2c20a519
mcbtay <mcbtay@gmail.com>
2022-06-07 22:40:35 -0400

    adding unity csproj to solution and moving away from it being in unity

---
+++ XLGearModifier.Unity/XLGMWhatsEquippedLineItem.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace XLGearModifier.Unity
+{
+    public class UnityEventString : UnityEvent<string>
+    {
+    }
+
+    public class XLGMWhatsEquippedLineItem : MonoBehaviour
+	{
+		public Image image;
+		public TMP_Text meshName;
+		public TMP_Text textureName;
+		public TMP_Text creatorName;
+
+        [HideInInspector] 
+        public UnityEventString equippedLineClicked;
+
+        public XLGMWhatsEquippedLineItem()
+        {
+            equippedLineClicked = new UnityEventString();
+        }
+
+		public void OnClick()
+        {
+			equippedLineClicked.Invoke(meshName.text);
+        }
+	}
+}
